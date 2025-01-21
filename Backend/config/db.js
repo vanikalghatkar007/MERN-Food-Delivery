4d87e1f91cd0b92340817142ab9aaef346266c80
vanikalghatkar007 <vanikalghatkar@gmail.com>
2025-01-21 19:49:39 +0530

    bug fix

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDb = async () => {
-    await mongoose.connect('mongodb+srv://vani:Vani1!@cluster0.sq32n.mongodb.net/food-delivery')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log('DB Connected'))
 }
